a649199fbf5c78d7ab5e561aa1071366a21fda2a
mogge <muzigen@gmail.com>
2018-09-05 07:42:05 +0900

    update main

--- main.go
+++ main.go
@@ -1,7 +1,106 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
 
-func main(){
-	fmt.Println("hello world")
+	"cloud.google.com/go/vision/apiv1"
+	"golang.org/x/net/context"
+
+	"log"
+)
+
+func textDetection(filePath string) {
+	ctx := context.Background()
+
+	client, err := vision.NewImageAnnotatorClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+	defer file.Close()
+	image, err := vision.NewImageFromReader(file)
+	if err != nil {
+		log.Fatalf("Failed to create image: %v", err)
+	}
+
+	texts, err := client.DetectTexts(ctx, image, nil, 10)
+	if err != nil {
+		log.Fatalf("Failed to detect labels: %v", err)
+	}
+
+	for _, text := range texts {
+		fmt.Println(text.Description)
+	}
+}
+
+// findLabels gets labels from the Vision API for an image at the given file path.
+func findLabels(file string) ([]string, error) {
+	// [START init]
+	ctx := context.Background()
+
+	// Create the client.
+	client, err := vision.NewImageAnnotatorClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	// [END init]
+
+	// [START request]
+	// Open the file.
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	image, err := vision.NewImageFromReader(f)
+	if err != nil {
+		return nil, err
+	}
+
+	// Perform the request.
+	annotations, err := client.DetectLabels(ctx, image, nil, 10)
+	if err != nil {
+		return nil, err
+	}
+	// [END request]
+	// [START transform]
+	var labels []string
+	for _, annotation := range annotations {
+		labels = append(labels, annotation.Description)
+	}
+	return labels, nil
+	// [END transform]
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s <path-to-image>\n", filepath.Base(os.Args[0]))
+	}
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	labels, err := findLabels(args[0])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+	if len(labels) == 0 {
+		fmt.Println("No labels found.")
+	} else {
+		fmt.Println("Found labels:")
+		for _, label := range labels {
+			fmt.Println(label)
+		}
+	}
 }
